8feb2ab85bac235ebad0554eccc1c4e306e2b36c
justingrimmer <jgrimmer@stanford.edu>
2018-09-06 00:38:36 -0700

    Updating Files

---
+++ mc3_18.R
@@ -0,0 +1,58 @@
+##putting together a function that approximates, via a taylor series.  
+
+
+
+approx_e<- function(a, x, order){
+	out<- 0
+	for(z in 0:order){
+	out<- out + (exp(a)/factorial(z))*(x - a)^z
+	}
+	return(out)
+	}
+
+
+x<- seq(-2, 5, len = 1000)
+
+plot(exp(x)~x, type='l', lwd = 3)
+
+lines(approx_e(1, x, 0)~x, col='red')
+lines(approx_e(1, x, 1)~x, col='red')
+lines(approx_e(1, x, 2)~x, col='red')
+lines(approx_e(1, x, 3)~x, col= 'red')
+lines(approx_e(1, x, 4)~x, col='red')
+lines(approx_e(1, x, 5)~x, col='red')
+lines(approx_e(1, x, 6)~x, col='red')
+lines(approx_e(1, x, 7)~x, col='red')
+lines(approx_e(1, x, 8)~x, col='red')
+
+
+approx_cos<- function(a, x, order){
+	out<- 0 
+	int<- 0 
+	vals<- c(cos(a), -sin(a), -cos(a), sin(a))
+	for(z in 0:order){
+		int<- int +1
+		out<- out + (vals[int]/factorial(z))*(x - a)^z
+		if(int==4){
+			int<- 0
+			}
+		}
+	return(out)
+	
+	}
+
+
+x<- seq(-10, 10, len = 1000)
+plot(cos(x)~x, type='l', lwd = 3)
+
+lines(approx_cos(0, x, 1)~x, col='red')
+lines(approx_cos(0, x, 2)~x, col='red')
+lines(approx_cos(0, x, 3)~x, col= 'red', lwd = 3)
+lines(approx_cos(0, x, 4)~x, col='red', lwd = 3)
+lines(approx_cos(0, x, 5)~x, col='red', lwd = 3)
+lines(approx_cos(0, x, 6)~x, col='red', lwd = 3)
+lines(approx_cos(0, x, 7)~x, col='red', lwd = 3)
+lines(approx_cos(0, x , 20)~x, col='red', lwd=3)
+lines(approx_cos(0, x, 100)~x, col='red', lwd = 3)
+
+
